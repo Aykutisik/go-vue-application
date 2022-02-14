7b939f8aaaae22d6fc53f38a777de710e3523581
Aykut isik <aykut.isik@modanisa.com>
2022-02-14 23:01:51 +0300

    Adding steps folder and todo.js

---
+++ todo/src/steps/todo.js
@@ -0,0 +1,76 @@
+const { When, Then, Given } = require("cucumber");
+
+Given('Empty ToDo list', function() {
+
+});
+
+When('I write {string} to text box and click to add button', function(string) {
+
+});
+
+Then('I should see {string} item in ToDo list', function(string) {
+
+});
+
+Given('ToDo list with {string} item', function(string) {
+
+});
+
+When('I write {string} to text box and click to add button', function(string) {
+
+});
+
+Then('I should see {string} insterted to ToDo list below {string}', function(string, string2) {
+
+});
+
+Given('ToDo list with {string} item', function(string) {
+
+});
+
+When('I click on {string} text', function(string) {
+
+});
+
+Then('I should see {string} item marked as {string}', function(string, string2) {
+
+});
+
+Given('ToDo list with marked {string} item', function(string) {
+
+});
+
+When('I click on {string} text', function(string) {
+
+});
+
+Then('I should see mark of {string} item should be cleared as {string}', function(string, string2) {
+
+});
+
+Given('ToDo list with {string} item', function(string) {
+
+});
+
+When('I click on delete button next to {string} item', function(string) {
+
+});
+
+
+Then('List should be empty', function() {
+
+});
+
+
+Given('ToDo list with {string} and {string} item in order', function(string, string2) {
+
+});
+
+
+When('I click on delete button next to {string} item', function(string) {
+
+});
+
+Then('I should see {string} item in ToDo list', function(string) {
+
+});
