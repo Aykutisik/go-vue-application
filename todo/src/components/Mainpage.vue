82393196003f2b4f61d05cdbfc6d9b61033baeef
Aykut isik <aykut.isik@modanisa.com>
2022-03-06 15:30:14 +0300

    open reload

--- todo/src/components/Mainpage.vue
+++ todo/src/components/Mainpage.vue
@@ -66,7 +66,7 @@ export default {
         })
         .catch((error) => console.log(error));
       
-      //location.reload();
+      location.reload();
     },
     AddTodo() {
       console.log("Add")
