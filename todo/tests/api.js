c9aaac1ca7dba0467664d7eaa614fa1e8af55f60
Furkan Küçük <furkan.kucuk@trhqmac150.local>
2022-03-07 15:46:23 +0300

    consumer test for createtodo

--- todo/tests/api.js
+++ todo/tests/api.js
@@ -11,3 +11,37 @@ export async function getTodos(baseURL) {
         },
     })
 }
+
+export async function createTodo(baseURL, todoElement){
+    return axios.request({
+        method: "POST",
+        url: baseURL + "/CreateTodo",
+        headers: {
+            'Accept': 'application/json; charset=utf-8',
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+        data: todoElement
+    })
+}
+
+export async function updateTodo(baseURL){
+    return axios.request({
+        method: "PUT",
+        url: baseURL + `/UpdateTodo`,
+        headers: {
+            'Accept': 'application/json; charset=utf-8',
+            'Content-type': 'application/json; charset=UTF-8',
+        }
+    })
+}
+
+export async function deleteTodo(baseURL, id){
+    return axios.request({
+        method: "PUT",
+        url: baseURL + `/DeleteTodo/${id}`,
+        headers: {
+            'Accept': 'application/json; charset=utf-8',
+            'Content-type': 'application/json; charset=utf-8',
+        }
+    })
+}
--- todo/tests/contract/consumer.spec.js
+++ todo/tests/contract/consumer.spec.js
@@ -1,5 +1,5 @@
 const { Pact } = require('@pact-foundation/pact')
-const { getTodos } = require('../api');
+const { getTodos, createTodo } = require('../api');
 
 import "babel-polyfill";
 
@@ -21,8 +21,9 @@ describe('Todo Api', () => {
     afterAll(() => provider.finalize());
 
     let addedTodo;
+
     describe('Get todo items', () => {
-        it("Should return all todo items", async() => {
+        it("Should return all todo items", async () => {
             const expectedResponse = [{ "_id": "62234346c2a65768f2c03ca5", "status": 0, "text": "drink water" }, { "_id": "6225bfaec2a65768f2c03ca6", "status": 0, "text": "bla bla" }]
 
             provider.addInteraction({
@@ -48,4 +49,36 @@ describe('Todo Api', () => {
 
         })
     })
+
+    describe("Create todo", () => {
+        it('Should add an element to todo list', async () => {
+    
+            const requestBody =
+                {"status": 0, "text": "new todo element" }
+    
+            provider.addInteraction({
+                state: "a todo item created",
+                uponReceiving: "create item request",
+                withRequest: {
+                    method: "POST",
+                    path: "/CreateTodo",
+                    headers: {
+                        'Accept': 'application/json; charset=utf-8',
+                        'Content-Type': 'application/json; charset=UTF-8'
+                    },
+                    body: requestBody
+                },
+                willRespondWith: {
+                    status: 201,
+                    headers: {
+                        'Content-Type': 'application/json; charset=UTF-8'
+                    },
+                }
+            })
+            let response = await createTodo(baseURL, requestBody);
+            const myJSON = JSON.stringify(response.statusText);
+            expect(myJSON).toEqual( "\"Created \"");
+        })
+    })
 })
+
